e68fcda49639a9cfa68124908a4f7d22c5533e5f
Roman Mukhin <romamuhino1@gmail.com>
2023-02-10 16:56:51 +0530

    step2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,8 @@ import {program } from 'commander';
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1', '-v', '-version');
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format, <type>', 'output format')
  
 program.parse();
