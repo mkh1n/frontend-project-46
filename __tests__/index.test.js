1a8b03678935206cb2a0f84f5d2689904ae99537
Roman Mukhin <romamuhino1@gmail.com>
2023-02-14 19:48:51 +0530

    :3

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,5 +4,5 @@ import gendiff from '../index.js';
 
 test('gendiff', () => {
     expect(gendiff('__tests__/__fixtures__/file1.json', '__tests__/__fixtures__/file2.json'))
-    .toEqual('{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true}')
+    .toEqual('{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}')
   });
